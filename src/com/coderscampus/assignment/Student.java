225b42a5cec321da1695fd18e021622663e79957
Sam Azizi <sadeghazizi.ca@gmail.com>
2023-01-14 23:46:24 -0800

    first commit
    with ignore null

---
+++ src/com/coderscampus/assignment/Student.java
@@ -0,0 +1,83 @@
+package com.coderscampus.assignment;
+
+public class Student implements Comparable<Student> {
+	private String studentId;
+	private String studentName;
+	private String courseName;
+	private Integer grade;
+
+	public Student(String studentId, String studentName, String courseName, Integer grade) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.courseName = courseName;
+		this.grade = grade;
+	}
+
+	/**
+	 * @return the studentId
+	 */
+	public String getStudentId() {
+		return studentId;
+	}
+
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	/**
+	 * @return the studentName
+	 */
+	public String getStudentName() {
+		return studentName;
+	}
+
+	/**
+	 * @param studentName the studentName to set
+	 */
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+
+	/**
+	 * @param courseName the courseName to set
+	 */
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	/**
+	 * @return the grade
+	 */
+	public Integer getGrade() {
+		return grade;
+	}
+
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(Integer grade) {
+		this.grade = grade;
+	}
+
+	@Override
+	public int compareTo(Student otherStudent) {
+		if ((this.grade > otherStudent.grade) || (otherStudent.getGrade() == null)) {
+			return -1;
+		} else if (this.grade < otherStudent.grade) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+}
---
+++ src/com/coderscampus/assignment/StudentApplication.java
@@ -0,0 +1,15 @@
+package com.coderscampus.assignment;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class StudentApplication {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		StudentServices studentservices = new StudentServices();
+		studentservices.splitStudentsByCourse();
+		studentservices.printStudentsByCourses("course1.csv");
+		studentservices.printStudentsByCourses("course2.csv");
+		studentservices.printStudentsByCourses("course3.csv");
+	}
+}
